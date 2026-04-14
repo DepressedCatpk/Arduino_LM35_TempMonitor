7151bd007ce3403bb04220821f0be78728031993
DepressedCatpk <nguyenkhoi30122005@gmail.com>
2026-04-14 18:45:28 +0700

    feat:ho tro doc 2 kenh LM35 (A0, A1)

---
+++ firmware/LM35_TempReader/LM35_TempReader.ino/LM35_TempReader/LM35_TempReader.ino
@@ -0,0 +1,26 @@
+/*
+ * Dự án: Đọc nhiệt độ 2 kênh LM35 và gửi qua Serial dạng CSV
+ * Môn: Mạng Cảm Biến - Tuần 03
+ */
+
+int adcValues[2]; 
+float nhietDo[2]; 
+char chuoi[30];
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+}
+
+void loop() {
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+
+  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+  Serial.print(chuoi);
+  
+  delay(100); 
+}
